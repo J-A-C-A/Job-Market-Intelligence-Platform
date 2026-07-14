0bad2c4e99429669007f111189217ff7d3e260aa
Jan Konczal <JanKonczal33@wp.pl>
2026-07-14 17:46:26 +0200

    dodanie pliku __init__.py ,w którym następuje zaimportowanie wszystkich plików z katalogu models

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.technology import Technology
+from app.models.company import Company
+from app.models.job_offer import JobOffer
+from app.models.offer_tech import OfferTech
+from app.models.offer_history import OfferHistory
