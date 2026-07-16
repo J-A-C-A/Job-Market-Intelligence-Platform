7caadbe018a2b0e8e8ca38936616b8b44fdd767d
Jan Konczal <JanKonczal33@wp.pl>
2026-07-16 15:51:30 +0200

    połączenie bazy PostgreSQL w database.py, dodanie alembic do projektu

--- app/database.py
+++ app/database.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+import dotenv
+import os
 
+dotenv.load_dotenv()
+DB_USER= os.getenv("DB_USER")
+DB_PASSWORD= os.getenv("DB_PASSWORD")
+DB_HOST= os.getenv("DB_HOST")
+DB_PORT= os.getenv("DB_PORT")
+DB_NAME= os.getenv("DB_NAME")
+connection= f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-connection = "sqlite:///data_base.db"
 engine = create_engine(connection)
 SessionLocal = sessionmaker(bind=engine)
 
