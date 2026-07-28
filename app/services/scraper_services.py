dd4c80a12a1c590f2d356d73ff1ca5ae350dad58
Jan Konczal <JanKonczal33@wp.pl>
2026-07-28 21:18:19 +0200

    dodanie funkcji która pobiera dane ze scrapera i zapisuje je w bazie

---
+++ app/services/scraper_services.py
@@ -0,0 +1,12 @@
+from app.scrapers.nofluffjobs_scraper import run_scraper
+from sqlalchemy.orm import Session
+from job_offer_service import process_scraped_offer
+
+def scrape_and_save_offers(db:Session) -> None:
+    offers = run_scraper()
+    for offer in offers:
+        try:
+           process_scraped_offer(db,offer)
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+
