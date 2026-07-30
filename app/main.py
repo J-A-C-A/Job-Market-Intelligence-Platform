ee1293bee4a1c1bbbdc518a83cee738498c99a78
Jan Konczal <JanKonczal33@wp.pl>
2026-07-30 18:58:17 +0200

    podłączenie endpointów ze statystykami do api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.api.job_offers import router
-
+from app.api.job_offers import router as r1
+from app.api.stats import router as r2
 app = FastAPI()
-app.include_router(router)
+app.include_router(r1)
+app.include_router(r2)
