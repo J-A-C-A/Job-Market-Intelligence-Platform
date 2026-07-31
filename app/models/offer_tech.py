09d4fe752f46586e3d34424f7d134ef5c5c6d5ee
Jan Konczal <JanKonczal33@wp.pl>
2026-07-31 12:43:05 +0200

    zmiana modelu tabeli offer_tech. dodano ograniczenie on_delete= cascade dla kolumny offer_id

--- app/models/offer_tech.py
+++ app/models/offer_tech.py
@@ -5,5 +5,5 @@ from app.database import Base
 
 class OfferTech(Base):
     __tablename__ = "offer_tech"
-    offer_id: Mapped[int] = mapped_column(ForeignKey("job_offers.offer_id"), primary_key=True)
+    offer_id: Mapped[int] = mapped_column(ForeignKey("job_offers.offer_id", ondelete="CASCADE"), primary_key=True)
     tech_id: Mapped[int] = mapped_column(ForeignKey("technologies.tech_id"), primary_key=True)
